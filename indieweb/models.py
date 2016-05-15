cddbdbb23a3a2c83c6061843d39138d39978e7b1
Jochen Wersdörfer <jochen@wersdoerfer.de>
2016-05-15 15:15:42 +0200

    added attributes to auth model

--- indieweb/models.py
+++ indieweb/models.py
@@ -6,11 +6,15 @@ from model_utils.models import TimeStampedModel
 
 
 class Auth(TimeStampedModel):
-    pass
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='indieweb_auth')
+    state = models.IntegerField()
+    client_id = models.CharField(max_length=512)
+    redirect_uri = models.CharField(max_length=1024)
+    scope = models.CharField(max_length=256)
+    me = models.CharField(max_length=512)
+    key = models.CharField(max_length=128)
     
 
 class Token(TimeStampedModel):
     pass
-    
-
-
