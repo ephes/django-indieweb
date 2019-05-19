b03bbfac5ab8524eac4396c9c2c7420edeb0151e
Jochen Wersdörfer <jochen@wersdoerfer.de>
2019-05-19 08:20:33 +0200

    improved setup.py

--- setup.py
+++ setup.py
@@ -17,62 +17,62 @@ def get_version(*file_paths):
                               version_file, re.M)
     if version_match:
         return version_match.group(1)
-    raise RuntimeError('Unable to find version string.')
+    raise RuntimeError("Unable to find version string.")
 
-version = get_version('indieweb', '__init__.py')
+version = get_version("indieweb", "__init__.py")
 
-if sys.argv[-1] == 'publish':
+if sys.argv[-1] == "publish":
     try:
         import wheel
+
+        print("Wheel version: ", wheel.__version__)
     except ImportError:
         print('Wheel library missing. Please run "pip install wheel"')
         sys.exit()
-    os.system('python setup.py sdist upload')
-    os.system('python setup.py bdist_wheel upload')
+    os.system("python setup.py sdist upload")
+    os.system("python setup.py bdist_wheel upload")
     sys.exit()
 
-if sys.argv[-1] == 'tag':
+if sys.argv[-1] == "tag":
     print("Tagging the version on github:")
     os.system("git tag -a %s -m 'version %s'" % (version, version))
     os.system("git push --tags")
     sys.exit()
 
-readme = open('README.rst').read()
-history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+readme = open("README.rst").read()
+history = open("HISTORY.rst").read().replace(".. :changelog:", "")
 
 setup(
-    name='django-indieweb',
+    name="django-indieweb",
     version=version,
     description="""includes indieauth and a micropub endpoint""",
-    long_description=readme + '\n\n' + history,
-    author='Jochen Wersdörfer',
-    author_email='jochen-indieweb@wersdoerfer.de',
-    url='https://github.com/ephes/django-indieweb',
-    packages=[
-        'indieweb',
-    ],
+    long_description=readme + "\n\n" + history,
+    author="Jochen Wersdörfer",
+    author_email="jochen-indieweb@wersdoerfer.de",
+    url="https://github.com/ephes/django-indieweb",
+    packages=["indieweb"],
     include_package_data=True,
     install_requires=[
-        
-            "django-model-utils>=2.0",
-        
+        "django-model-utils>=2.0",
+        "django-model-utils",
+        "django-braces",
     ],
     license="BSD",
     zip_safe=False,
-    keywords='django-indieweb',
+    keywords="django-indieweb",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Framework :: Django',
-        'Framework :: Django :: 1.8',
-        'Framework :: Django :: 1.9',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
 )
